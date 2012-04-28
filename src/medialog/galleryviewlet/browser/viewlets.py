0668eef5e6df5b5e6a1ba8926af208d4e79cfd3b
G4 <g4@g4s-power-mac-g5-3.local>
2012-04-28 14:27:47 +0200

    settings to get the path right and less code in the viewlet itself

--- src/medialog/galleryviewlet/browser/viewlets.py
+++ src/medialog/galleryviewlet/browser/viewlets.py
@@ -8,12 +8,12 @@ from Products.CMFCore.utils import getToolByName
 from medialog.galleryviewlet import galleryviewletMessageFactory as _
 from medialog.galleryviewlet.settings import GalleryviewletSettings
 from medialog.galleryviewlet.settings import IGalleryviewletSettings
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from zope.component import getMultiAdapter
 
 #dont know if all this is needed
 from Products.Five import BrowserView
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
-from zope.component import getMultiAdapter
 
 
 
@@ -21,9 +21,20 @@ class GalleryViewlet(ViewletBase):
     render = ViewPageTemplateFile('galleryviewlet.pt')
     
     implements(IGalleryviewletSettings)
-
-    def gallery(self):
+    
+    @property
+    @memoize
+    def gallerypath(self):
 		context=self.context
 		self.settings = GalleryviewletSettings(context)
-		gallerypath = self.settings.gallerypath
-		return  gallerypath
+		
+		try:
+			portal_state = getMultiAdapter((self.context, self.request), name=u'plone_portal_state')
+			portal = portal_state.portal()
+			path = str(self.settings.gallerypath)
+			if path.startswith('/'):
+				path = path[1:]
+				
+			return portal.restrictedTraverse(path, default=False)
+		except:
+			return False
